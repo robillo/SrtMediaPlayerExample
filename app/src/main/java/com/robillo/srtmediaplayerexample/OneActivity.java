6344de9f9226dba5a429f79e75f579e578541ca4
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-12-27 23:49:36 +0530

    raw folder files (star.mp3 and star_srt.srt)

--- app/src/main/java/com/robillo/srtmediaplayerexample/OneActivity.java
+++ app/src/main/java/com/robillo/srtmediaplayerexample/OneActivity.java
@@ -1,5 +1,6 @@
 package com.robillo.srtmediaplayerexample;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class OneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        MediaPlayer mPlayer = MediaPlayer.create(OneActivity.this, R.raw.star);
+        mPlayer.start();
     }
 }
