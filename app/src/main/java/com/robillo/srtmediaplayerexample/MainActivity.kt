985a052c22de4333872429a7e8dbd463b2241ab1
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-12-27 23:36:25 +0530

    post-initial commit

--- app/src/main/java/com/robillo/srtmediaplayerexample/MainActivity.kt
+++ app/src/main/java/com/robillo/srtmediaplayerexample/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.robillo.srtmediaplayerexample
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        activityOne.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this, OneActivity::class.java))
+        })
     }
 }
---
+++ app/src/main/java/com/robillo/srtmediaplayerexample/OneActivity.java
@@ -0,0 +1,13 @@
+package com.robillo.srtmediaplayerexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
