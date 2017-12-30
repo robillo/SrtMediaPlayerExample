63c0f3576a1802b5530884f857f80138e991e70a
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-12-30 19:28:00 +0530

    play pause controls

--- app/src/main/java/com/robillo/srtmediaplayerexample/FourActivity.kt
+++ app/src/main/java/com/robillo/srtmediaplayerexample/FourActivity.kt
@@ -3,15 +3,21 @@ package com.robillo.srtmediaplayerexample
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.github.ybq.android.spinkit.Style
 import com.github.ybq.android.spinkit.style.Wave
 
 import com.halilibo.bettervideoplayer.BetterVideoCallback
 import com.halilibo.bettervideoplayer.BetterVideoPlayer
+import com.halilibo.bettervideoplayer.BetterVideoProgressCallback
 import com.halilibo.bettervideoplayer.subtitle.CaptionsView
 import kotlinx.android.synthetic.main.activity_four.*
 
-class FourActivity : AppCompatActivity(), BetterVideoCallback {
+class FourActivity : AppCompatActivity(), BetterVideoCallback, BetterVideoProgressCallback {
+
+    override fun onVideoProgressUpdate(position: Int, duration: Int) {
+
+    }
 
     private val TEST_URL = "http://readrush.in/audio/subtle/subtle_open.mp3"
 
@@ -19,15 +25,32 @@ class FourActivity : AppCompatActivity(), BetterVideoCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_four)
 
-        player.setCallback(this);
+        player.setLoop(false)
         player.setLoadingStyle(2)
+        player.disableControls()
 
         player.setCaptions(Uri.parse("http://readrush.in/subtitles/subtle/subtle_0.srt"), CaptionsView.CMime.SUBRIP)
-
-        // Sets the source to the HTTP URL held in the TEST_URL variable.
-        // To play files, you can use Uri.fromFile(new File("..."))
         player.setSource(Uri.parse(TEST_URL));
+        player.setProgressCallback(this)
 
+        player.setCallback(this)
+
+        play_pause.setOnClickListener {
+            if(player.isPrepared){
+                if(player.isPlaying){
+                    player.pause()
+                }
+                else{
+                    player.start()
+                }
+            }
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        player.start()
     }
 
     public override fun onPause() {
@@ -42,11 +65,11 @@ class FourActivity : AppCompatActivity(), BetterVideoCallback {
     }
 
     override fun onStarted(player: BetterVideoPlayer) {
-
+        play_pause.setImageDrawable(resources.getDrawable(R.drawable.ic_pause_circle_filled_black_24dp))
     }
 
     override fun onPaused(player: BetterVideoPlayer) {
-
+        play_pause.setImageDrawable(resources.getDrawable(R.drawable.ic_play_circle_filled_black_24dp))
     }
 
     override fun onPreparing(player: BetterVideoPlayer) {
@@ -54,7 +77,9 @@ class FourActivity : AppCompatActivity(), BetterVideoCallback {
     }
 
     override fun onPrepared(player: BetterVideoPlayer) {
-
+        play_pause.visibility = View.VISIBLE
+        play_pause.isClickable = true
+        play_pause.setImageDrawable(resources.getDrawable(R.drawable.ic_play_circle_filled_black_24dp))
     }
 
     override fun onBuffering(percent: Int) {
